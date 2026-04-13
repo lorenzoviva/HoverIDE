217252f975d36d003379ade6172574bbfe24eda6
lorenzoviva <lorenzo.vaccaro@hotmail.com>
2026-04-13 23:12:10 +0200

    test commit

---
+++ .hoverscripts/example.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Error</title>
+</head>
+<body>
+<pre>Error: ENOENT: no such file or directory, open &#39;H:\projects\HoverIDESandboxes\HoverIDE\.hoverscripts\example.js&#39;<br> &nbsp; &nbsp;at Object.readFileSync (node:fs:441:20)<br> &nbsp; &nbsp;at FileSystemAdapter.read (file:///H:/projects/HoverIDE/backend/adapters/FileSystemAdapter.js:7:19)<br> &nbsp; &nbsp;at file:///H:/projects/HoverIDE/backend/api/file.routes.js:21:37<br> &nbsp; &nbsp;at Layer.handle [as handle_request] (H:\projects\HoverIDE\backend\node_modules\express\lib\router\layer.js:95:5)<br> &nbsp; &nbsp;at next (H:\projects\HoverIDE\backend\node_modules\express\lib\router\route.js:149:13)<br> &nbsp; &nbsp;at Route.dispatch (H:\projects\HoverIDE\backend\node_modules\express\lib\router\route.js:119:3)<br> &nbsp; &nbsp;at Layer.handle [as handle_request] (H:\projects\HoverIDE\backend\node_modules\express\lib\router\layer.js:95:5)<br> &nbsp; &nbsp;at H:\projects\HoverIDE\backend\node_modules\express\lib\router\index.js:284:15<br> &nbsp; &nbsp;at Function.process_params (H:\projects\HoverIDE\backend\node_modules\express\lib\router\index.js:346:12)<br> &nbsp; &nbsp;at next (H:\projects\HoverIDE\backend\node_modules\express\lib\router\index.js:280:10)</pre>
+</body>
+</html>
